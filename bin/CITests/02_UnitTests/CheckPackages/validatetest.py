9d7bf81d36e1a056d074fbaef6a8185de3e87d09
David Jansen <david.jansen@eonerc.rwth-aachen.de>
2022-08-23 16:52:58 +0200

    Update validatetest.py

--- bin/CITests/02_UnitTests/CheckPackages/validatetest.py
+++ bin/CITests/02_UnitTests/CheckPackages/validatetest.py
@@ -188,7 +188,7 @@ class ValidateTest(object):
                     ex_file = open(filepath, "r", encoding='utf8', errors='ignore')
                     lines = ex_file.readlines()
                     for line in lines:
-                        # if line.find("extends") > -1 and line.find("Modelica.Icons.Example") > -1:
+                        if line.find("extends") > -1 and line.find("Modelica.Icons.Example") > -1:
                         example = filepath.replace(os.sep, ".")
                         example = example[example.rfind(self.mo_library):example.rfind(".mo")]
                         example_list.append(example)
