fec3921571d96835d1f9551bda62d9fdcfaee19e
fabian.wuellhorst <fabian.wuellhorst@eonerc.rwth-aachen.de>
2024-06-16 23:13:16 +0200

    fix args

--- ci-tests/scripts/local_windows_test.bat
+++ ci-tests/scripts/local_windows_test.bat
@@ -7,24 +7,24 @@
 python -m ModelicaPyCI.syntax.html_tidy --filter-whitelist-flag  --correct-view-flag  --log-flag  --whitelist-library IBPSA --library BESMod --packages Examples Tutorial Utilities Systems 
 python -m ModelicaPyCI.syntax.html_tidy --filter-whitelist-flag  --correct-view-flag  --log-flag  --whitelist-library IBPSA --library BESMod --packages Examples Tutorial Utilities Systems 
 
-python -m ModelicaPyCI.syntax.style_checking --dymola-version 2022 --library BESMod 
-python -m ModelicaPyCI.syntax.style_checking --changed-flag  --dymola-version 2022 --library BESMod 
+python -m ModelicaPyCI.syntax.style_checking --startup-mos startup.mos --dymola-version 2022 --library BESMod 
+python -m ModelicaPyCI.syntax.style_checking --changed-flag  --startup-mos startup.mos --dymola-version 2022 --library BESMod 
 
 python -m ModelicaPyCI.syntax.naming_guideline --changed-flag  --gitlab-page https://ebc.pages.rwth-aachen.de/EBC_all/github_ci/BESMod --github-token ${GITHUB_API_TOKEN} --working-branch $CI_COMMIT_BRANCH --github-repository RWTH-EBC/BESMod --main-branch main --config ci-tests/naming_guideline.toml --library BESMod 
 
 :: Check & Simulate BESMod Examples on PR
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Examples
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Examples
 :: Check & Simulate BESMod Examples on push
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Examples
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Examples
 :: Check & Simulate BESMod Tutorial on PR
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Tutorial
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Tutorial
 :: Check & Simulate BESMod Tutorial on push
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Tutorial
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Tutorial
 :: Check & Simulate BESMod Utilities on PR
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Utilities
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Utilities
 :: Check & Simulate BESMod Utilities on push
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Utilities
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Utilities
 :: Check & Simulate BESMod Systems on PR
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Systems
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Systems
 :: Check & Simulate BESMod Systems on push
-python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Systems
+python -m ModelicaPyCI.unittest.checkpackages.validatetest --dym-options DYM_SIM DYM_CHECK --changed-flag  --startup-mos startup.mos --dymola-version 2022 --additional-libraries-to-load  --library BESMod  --packages Systems
