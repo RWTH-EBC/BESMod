70dde02621bb05e869fad9ad4a0d66d0837ff01e
felix.stegemerten <felix.stegemerten@eonerc.rwth-aachen.de>
2026-01-14 14:26:21 +0100

    Fix tests

--- bes-rules/tests/test_buildings.py
+++ bes-rules/tests/test_buildings.py
@@ -8,12 +8,11 @@ from pathlib import Path
 
 import numpy as np
 
-from bes_rules.configs.inputs import InputConfig, UserProfile, BuildingConfig
-from bes_rules.boundary_conditions.building import create_buildings, get_building_configs_by_name
-from bes_rules.boundary_conditions.weather import (
-    get_building_configs_by_name,
-    get_supply_temperature_after_retrofit
-)
+from bes_rules.configs.inputs import InputConfig, UserProfile
+from bes_rules.boundary_conditions.building import (create_buildings, get_building_configs_by_name,
+                                                    get_supply_temperature_after_retrofit,
+                                                    get_retrofit_temperatures)
+from bes_rules.boundary_conditions.weather import get_weather_configs_by_names
 
 
 class TestRetrofitTemperatures(unittest.TestCase):
@@ -21,13 +20,14 @@ class TestRetrofitTemperatures(unittest.TestCase):
 
     def test_no_retrofit(self):
         regression_tests = {
-            "NoRetrofit1918": (90 + 273.15, 20),
-            "NoRetrofit1983": (70 + 273.15, 15)
+            "NoRetrofit1918": (363.15, 20, 363.15, 20, 18592.321522476486, 18592.321522476486),
+            "NoRetrofit1983": (328.15, 10, 328.15, 10, 9205.351783448908, 9205.351783448908)
         }
         buildings = get_building_configs_by_name(building_names=list(regression_tests.keys()))
         configs = self._create_input_configs(buildings=buildings)
         for config in configs:
-            result = config.building.get_retrofit_temperatures(
+            result = get_retrofit_temperatures(
+                building_config=config.building,
                 TOda_nominal=config.weather.TOda_nominal,
                 TRoom_nominal=config.user.room_set_temperature
             )
@@ -39,31 +39,29 @@ class TestRetrofitTemperatures(unittest.TestCase):
             regression_data = json.load(file)
         TSup1 = np.linspace(273.15 + 35, 273.15 + 90, 20)
         dT1 = np.linspace(5, 20, 20)
-        TRet1 = TSup1 - dT1
         QNom1 = 100
         QNom2_arr = [20, 40, 60, 80, 100]
         for idx, QNom2 in enumerate(QNom2_arr):
-            TSup2, dT2 = get_supply_temperature_after_retrofit(
-                TRoom1=293.15,
-                TRoom2=293.15,
-                n=1.3,
-                TSup1=TSup1,
-                TRet1=TRet1,
-                QNom1=QNom1,
-                QNom2=QNom2
-            )
-            self.assertEqual(list(TSup2), regression_data[str(QNom2)]["TSup2"])
-            self.assertEqual(list(dT2), regression_data[str(QNom2)]["dT2"])
-
-    def test_not_loaded_building(self):
-        buildings = get_building_configs_by_name(building_names=["Retrofit1918"])
-        config = self._create_input_configs(buildings=buildings, with_create_buildings=False)[0]
-        with self.assertRaises(ValueError):
-            config.building.get_modelica_modifier(input_config=config)
+            TSup2_list = []
+            dT2_list = []
+            for TSup, dT in zip(TSup1, dT1):
+                TSup2, dT2 = get_supply_temperature_after_retrofit(
+                    TRoom1=293.15,
+                    TRoom2=293.15,
+                    n=1.3,
+                    TSup1=TSup,
+                    TRet1=TSup - dT,
+                    QNom1=QNom1,
+                    QNom2=QNom2
+                )
+                TSup2_list.append(TSup2)
+                dT2_list.append(dT2)
+            self.assertEqual(TSup2_list, regression_data[str(QNom2)]["TSup2"])
+            self.assertEqual(dT2_list, regression_data[str(QNom2)]["dT2"])
 
     @staticmethod
     def _create_input_configs(buildings, with_create_buildings: bool = True):
-        weather = get_weather_configs_by_names(region_names=["Essen"])[0]
+        weather = get_weather_configs_by_names(region_names=["Potsdam"])[0]
         weather.TOda_nominal = 273.15 - 12
         if with_create_buildings:
             buildings = create_buildings(name="Retro", building_configs=buildings, export=False)
